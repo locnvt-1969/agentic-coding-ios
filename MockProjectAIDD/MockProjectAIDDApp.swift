e3b6a009f2c156f66085daad7f3af72e935c6314
nguyen.vuong.thanh.loc <nguyen.vuong.thanh.loc@sun-asterisk.com>
2026-05-27 13:23:54 +0700

    Initial iOS project setup

---
+++ MockProjectAIDD/MockProjectAIDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockProjectAIDDApp.swift
+//  MockProjectAIDD
+//
+//  Created by Thành Lộc Nguyễn Vương on 27/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MockProjectAIDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
